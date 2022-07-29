ea33e56f1f9fea7097dade7543fe6e4f34a2c16c
Gianluca Bredice - Desktop PC <bredicegian@gmail.com>
2022-07-29 20:11:02 -0300

    first shopping cart withhout events

--- js/script.js
+++ js/script.js
@@ -4,77 +4,32 @@ const mainMenuElement=document.getElementById('navbar')
 toggleMenuElement.addEventListener('click', ()=>{
     mainMenuElement.classList.toggle('navbar--show')
 })
+/*------------------ACCESO AL DOM--------------------*/
+const combos=document.getElementsByClassName('sale-container__item')
+const nameCombos=document.getElementsByClassName('sale-container__item-name')
+const codCombos=document.getElementsByClassName('sale-container__item-cod__item')
+const priceCombos=document.getElementsByClassName('sale-container__item-price__value')
+const containerCarrito=document.getElementById('carrito-container')
+const totalCarrito=document.getElementById('carrito-total')
+let totalCompra=0
+let salida=''
+while(!salida.toUpperCase().includes('N')){
+    let comboSeleccionado=prompt('Ingrese el número de combo que desea agregar al carrito: ')
+    if(comboSeleccionado<combos.length && comboSeleccionado>0){
+        for(let i=0; i<combos.length-1; i++){
+            if(comboSeleccionado==codCombos[i].innerHTML){
+                totalCompra+=parseInt(priceCombos[i].innerHTML)
+                let itemCarrito=document.createElement('div')
+                itemCarrito.innerHTML=`<p>${nameCombos[i].innerHTML}</p>
+                                                           <b>$${priceCombos[i].innerHTML}</b>`;
+                itemCarrito.className='carrito-container__item d-flex flex-row align-items-center justify-content-between p-4 gap-4'
+                containerCarrito.appendChild(itemCarrito)
+            }
+        }
+        salida=prompt('Desea agregar otra cosa al carrito?')
+    } else{
+        alert('Código no válido')
+    }
+}
+totalCarrito.innerHTML=`TOTAL $${totalCompra}`
 /*---------------------------------------------------------*/
-/*
-class Bebida{
-    constructor(cod, nombre,  precio){
-        this.cod=cod
-        this.nombre=nombre
-        this.precio=precio
-    }
-    infoBebida(){
-        return this.cod+') '+this.nombre+' - $'+this.precio
-    }
-    getPrecio(){
-        return this.precio
-    }
-}
-const informacionBebidas=(bebidas)=>{
-    let info=''
-    for(b of bebidas){
-        info+=b.infoBebida()+'\n'
-    }
-    return info
-}
-const b1=new Bebida(1, 'Fernet Branca', 1800)
-const b2=new Bebida(2, 'Vodka Smirnoff', 1300)
-const b3=new Bebida(3, 'Cerveza Sneijder', 280)
-const b4=new Bebida(4, `Gin Hendrick's`, 1900)
-const b5=new Bebida(5, 'Campari', 1250)
-const bebidas=[b1,b2,b3,b4,b5]
-let finCompra='Si'
-alert('¡Bienvenido a CASATOMADA!\nA continuacion se le mostrará las bebidas de las cuales disponemos.')
-while(!finCompra.toUpperCase().includes('N')){
-    const bebidasSeleccionadas=[]
-    let codB=parseInt(prompt('Indique el codigo de la bebida que desea o escriba -1 para salir: \n'+informacionBebidas(bebidas)))
-    while(codB!==-1){
-        switch(codB){
-            case 1:
-                bebidasSeleccionadas.push(b1)
-                alert('Bebida agregadada correctamente.')
-                break;
-            case 2:
-                bebidasSeleccionadas.push(b2)
-                alert('Bebida agregadada correctamente.')
-                break;
-            case 3:
-                bebidasSeleccionadas.push(b3)
-                alert('Bebida agregadada correctamente.')
-                break;
-            case 4:
-                bebidasSeleccionadas.push(b4)
-                alert('Bebida agregadada correctamente.')
-                break;
-            case 5:
-                bebidasSeleccionadas.push(b5)
-                alert('Bebida agregadada correctamente.')
-                break;
-            default:
-                alert('Código no válido.')
-                break;
-        }
-        codB=parseInt(prompt('Indique el codigo de la bebida que desea o escriba -1 para salir: \n'+informacionBebidas(bebidas)))
-    }
-    const totalAPagar=bebidasSeleccionadas.reduce((total, bebida) => total + bebida.getPrecio(),0) //REEMPLAZO DE FUNCION PARA CALUCULAR EL TOTAL DE LA COMPRA
-    bebidasSeleccionadas.sort((a,b) =>{ //ORDENA LAS BEBIDAS DE FORMA ASCENDENTE CON RESPECTO AL PRECIO
-        if(a.getPrecio()>b.getPrecio()){
-            return 1
-        }
-        if(a.getPrecio()<b.getPrecio()){
-            return -1
-        }
-        return 0
-    })
-    alert('Las bebidas seleccionadas fueron: \n'+informacionBebidas(bebidasSeleccionadas)+'\nTotal a pagar: $'+totalAPagar)
-    finCompra=prompt('Desea realizar otra compra?')
-}*/
