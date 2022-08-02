046320ae0552ed9b6d6682d4a4d1e34b4b39c1ca
Gianluca Bredice - Desktop PC <bredicegian@gmail.com>
2022-08-02 11:37:13 -0300

    change method for store items on shopping cart container

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ const mainMenuElement=document.getElementById('navbar')
 toggleMenuElement.addEventListener('click', ()=>{
     mainMenuElement.classList.toggle('navbar--show')
 })
+class Item{
+    constructor(name, price){
+        this.name=name
+        this.price=price
+    }
+}
+let cart=[]
 /*------------------ACCESO AL DOM--------------------*/
 const items=document.getElementsByClassName('shop-container__items-item')
 const nameItem=document.getElementsByClassName('shop-container__items-item__info-name')
@@ -15,17 +22,20 @@ const cartShopping_btnShowContainer=document.getElementById('cartShoppingBtnShow
 const counterShopping=document.getElementById('counterShopping')
 const cartContainer=document.getElementById('cartShopping-container__items')
 const cartTotal=document.getElementById('cartShopping-container__total')
+/*--------------------------------------------------------*/
 let totalCarrito=0;
 for(let i=0; i<items.length; i++){
     cartShoppingButton[i].addEventListener('click', ()=>{
+        const item=new Item(nameItem[i].innerHTML, priceItem[i].innerHTML)
+        cart.push(item) //ACA TRATE DE HACER LO QUE ME COMENTASTE EN EL ANTERIOR DESAFIO PERO DESPUES CUANDO LO QUIERO RECORRER Y AGREGAR EL HTML EN EL CONTAINER DEL CARRITO NO ME FUNCIONA.
         counterShopping.innerHTML=parseInt(counterShopping.innerHTML) + 1
-        totalCarrito+=parseInt(priceItem[i].innerHTML)
+        totalCarrito+=parseInt(item.price)
         let itemCarrito=document.createElement('div')
-        itemCarrito.innerHTML=`<p class="nameItem">${nameItem[i].innerHTML}</p>
-                                                   <b class="priceItem">$${priceItem[i].innerHTML}<b>`
+        itemCarrito.innerHTML=`<p class="nameItem">${item.name}</p>
+                                                   <b class="priceItem">$${item.price}<b>`
         itemCarrito.className='cartShopping-container__items-item d-flex align-items-start justify-content-between p-4 gap-4'
         cartContainer.appendChild(itemCarrito)
-        cartTotal.innerHTML=`TOTAL $${totalCarrito}`
+        cartTotal.innerHTML=`TOTAL $${totalCarrito}`  
     })
 }
 cartShopping_btnShowContainer.addEventListener('click', ()=>{
