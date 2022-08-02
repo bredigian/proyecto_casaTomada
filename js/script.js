f7c2882e730f24bc5f94ab5ea0fe98d66dacff34
Gianluca Bredice - Desktop PC <bredicegian@gmail.com>
2022-08-01 23:38:55 -0300

    delete comments and items innecesary

--- js/script.js
+++ js/script.js
@@ -8,13 +8,11 @@ toggleMenuElement.addEventListener('click', ()=>{
 const items=document.getElementsByClassName('shop-container__items-item')
 const nameItem=document.getElementsByClassName('shop-container__items-item__info-name')
 const priceItem=document.getElementsByClassName('price-num')
-
 const cartShoppingButton=document.getElementsByClassName('shop-container__items-item__icon')
 const cartShopping=document.getElementById('cartShopping')
 const cartShopping_container=document.getElementById('cartShopping-container')
 const cartShopping_btnShowContainer=document.getElementById('cartShoppingBtnShowContainer')
 const counterShopping=document.getElementById('counterShopping')
-
 const cartContainer=document.getElementById('cartShopping-container__items')
 const cartTotal=document.getElementById('cartShopping-container__total')
 let totalCarrito=0;
@@ -34,40 +32,3 @@ cartShopping_btnShowContainer.addEventListener('click', ()=>{
     cartShopping_container.classList.toggle('cartShopping-container--show')
     cartShopping_btnShowContainer.classList.toggle('cartShopping-btnShowContainer--rotate')
 })
-
-
-/*for(let i=1; i<cartShoppingButton.length; i++){
-    if(cartShopping.classList.value('cartShopping')){
-    }
-}
-const item=document.getElementsByClassName('shop-container__items-item')
-
-/*--------------------------------------------------------*/
-/*const combos=document.getElementsByClassName('sale-container__item')
-const nameCombos=document.getElementsByClassName('sale-container__item-name')
-const codCombos=document.getElementsByClassName('sale-container__item-cod__item')
-const priceCombos=document.getElementsByClassName('sale-container__item-price__value')
-const containerCarrito=document.getElementById('carrito-container')
-const totalCarrito=document.getElementById('carrito-total')
-let totalCompra=0
-let salida=''
-while(!salida.toUpperCase().includes('N')){
-    let comboSeleccionado=prompt('Ingrese el número de combo que desea agregar al carrito: ')
-    if(comboSeleccionado<combos.length && comboSeleccionado>0){
-        for(let i=0; i<combos.length-1; i++){
-            if(comboSeleccionado==codCombos[i].innerHTML){
-                totalCompra+=parseInt(priceCombos[i].innerHTML)
-                let itemCarrito=document.createElement('div')
-                itemCarrito.innerHTML=`<p>${nameCombos[i].innerHTML}</p>
-                                                           <b>$${priceCombos[i].innerHTML}</b>`;
-                itemCarrito.className='carrito-container__item d-flex flex-row align-items-center justify-content-between p-4 gap-4'
-                containerCarrito.appendChild(itemCarrito)
-            }
-        }
-        salida=prompt('Desea agregar otra cosa al carrito?')
-    } else{
-        alert('Código no válido')
-    }
-}
-totalCarrito.innerHTML=`TOTAL $${totalCompra}`*/
-/*---------------------------------------------------------*/
